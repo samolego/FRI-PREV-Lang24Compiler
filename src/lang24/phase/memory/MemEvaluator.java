f07fd2ebc27ee1c7b2fd9d6bb8f8bd349ed9e861
samolego <34912839+samolego@users.noreply.github.com>
2024-04-02 11:40:13 +0200

    More or less finnish memeval

--- src/lang24/phase/memory/MemEvaluator.java
+++ src/lang24/phase/memory/MemEvaluator.java
@@ -4,29 +4,37 @@ import lang24.common.report.Report;
 import lang24.data.ast.tree.*;
 import lang24.data.ast.tree.defn.*;
 import lang24.data.ast.tree.expr.*;
-import lang24.data.ast.tree.stmt.*;
 import lang24.data.ast.tree.type.*;
 import lang24.data.ast.visitor.*;
 import lang24.data.mem.*;
 import lang24.data.type.*;
+import lang24.phase.lexan.LexAn;
+import lang24.phase.seman.NameResolver;
 import lang24.phase.seman.SemAn;
+import lang24.phase.synan.SynAn;
+
+import static java.lang.Math.max;
+
 
 /**
  * Computing memory layout: stack frames and variable accesses.
- * 
+ *
  * @author bostjan.slivnik@fri.uni-lj.si
  */
-public class MemEvaluator implements AstFullVisitor<Long, Integer> {
+public class MemEvaluator implements AstFullVisitor<Object, Integer> {
+
+    private long maxCallSize = getSizeInBytes(SemPointerType.type);  // Static link always included
 
     /**
      * Gets the size of a type in bytes.
+     *
      * @param type The type to get the size of.
      * @return The size of the type in bytes.
      */
     private static long getSizeInBytes(SemType type) {
         return switch (type) {
             case SemPointerType ignored -> 8;
-            case SemIntType ignored -> 8;
+            case SemIntType ignored -> 4;
             case SemBoolType ignored -> 1;
             case SemCharType ignored -> 1;
             case SemStructType semStructType -> {
@@ -39,7 +47,7 @@ public class MemEvaluator implements AstFullVisitor<Long, Integer> {
             case SemUnionType semUnionType -> {
                 long size = 0;
                 for (var cmp : semUnionType.cmpTypes) {
-                    size = Math.max(size, getSizeInBytes(cmp));
+                    size = max(size, getSizeInBytes(cmp));
                 }
                 yield size;
             }
@@ -51,11 +59,16 @@ public class MemEvaluator implements AstFullVisitor<Long, Integer> {
 
     /**
      * Get the size of a type in bytes, rounded up to the nearest multiple of 8.
+     *
      * @param type The type to get the size of.
      * @return The size of the type in bytes, rounded up to the nearest multiple of 8.
      */
     private static long getRoundedSizeInBytes(SemType type) {
         long size = getSizeInBytes(type);
+        return roundTo8(size);
+    }
+
+    private static long roundTo8(long size) {
         if (size % 8 != 0) {
             size += 8 - size % 8;
         }
@@ -64,183 +77,149 @@ public class MemEvaluator implements AstFullVisitor<Long, Integer> {
     }
 
     @Override
-    public Long visit(AstNodes<? extends AstNode> nodes, Integer depth) {
+    public Object visit(AstNodes<? extends AstNode> nodes, Integer depth) {
         if (depth == null) {
             depth = 0;
         }
         return AstFullVisitor.super.visit(nodes, depth);
     }
 
-    @Override
-    public Long visit(AstTypDefn typDefn, Integer depth) {
-        return AstFullVisitor.super.visit(typDefn, depth);
-    }
 
     @Override
-    public Long visit(AstVarDefn varDefn, Integer depth) {
+    public Object visit(AstVarDefn varDefn, Integer depth) {
         // Get type of the variable
         var type = SemAn.ofType.get(varDefn);
 
         // Get the size of the type in bytes
-        long size = getRoundedSizeInBytes(type);
+        long size = getSizeInBytes(type);
 
-        MemAccess memAcc;
         if (depth == 0) {
             // Static variable definition
-            memAcc = new MemAbsAccess(size, new MemLabel(varDefn.name));
-        } else {
-            memAcc = new MemRelAccess(size, 0, depth);
+            var memAcc = new MemAbsAccess(size, new MemLabel(varDefn.name));
+            Memory.varAccesses.put(varDefn, memAcc);
         }
 
-        Memory.varAccesses.put(varDefn, memAcc);
-
-        // Shouldn't need? todo
         varDefn.type.accept(this, depth);
-
 
         return size;
     }
 
     @Override
-    public Long visit(AstFunDefn funDefn, Integer depth) {
-        long argsSize = funDefn.pars.accept(this, depth + 1);
-        funDefn.defns.accept(this, depth + 1);
+    public Object visit(AstFunDefn funDefn, Integer depth) {
 
-        long blockSize = 0;
-        if (funDefn.stmt != null) {
-            blockSize = funDefn.stmt.accept(this, depth + 1);
+        long paramSize = 0;
+        for (var par : funDefn.pars) {
+            par.accept(this, depth);
+
+            var type = SemAn.ofType.get(par);
+            // Round parameters & arguments
+            long size = getRoundedSizeInBytes(type);
+
+            var memAcc = new MemRelAccess(size, paramSize, depth);
+            Memory.parAccesses.put(par, memAcc);
+
+            paramSize += size;
         }
 
-        long size = argsSize + blockSize;
-        var frame = new MemFrame(new MemLabel(funDefn.name), depth, blockSize, argsSize, size);
+
+        long blockSize = 0;
+        for (var defn : funDefn.defns) {
+            defn.accept(this, depth);
+
+            if (defn instanceof AstVarDefn varDefn) {
+                var type = SemAn.ofType.get(defn);
+                long size = getSizeInBytes(type);
+                blockSize += size;
+
+                // Just variables interest us
+                // Automatic variable definition
+                var memAcc = new MemRelAccess(size, -blockSize, depth);
+
+                Memory.varAccesses.put(varDefn, memAcc);
+            }
+        }
+
+
+        this.maxCallSize = getSizeInBytes(SemPointerType.type);
+        if (funDefn.stmt != null) {
+            funDefn.stmt.accept(this, depth + 1);
+        }
+
+        long size = blockSize + this.maxCallSize + 2 * getSizeInBytes(SemPointerType.type); // one for return address, one for old frame pointer
+
+        boolean isNested = depth > 0;
+        MemLabel label = isNested ? new MemLabel() : new MemLabel(funDefn.name());
+
+        var frame = new MemFrame(label, depth, blockSize, this.maxCallSize, size);
+
         Memory.frames.put(funDefn, frame);
 
+
+        return size;
+    }
+
+    // todo
+    @Override
+    public Object visit(AstAtomExpr atomExpr, Integer depth) {
+        if (atomExpr.type == AstAtomExpr.Type.STR) {
+            assert SemAn.ofType.get(atomExpr) == SemPointerType.stringType : "Wrong string pointer for node " + atomExpr.getText();
+            Memory.strings.put(atomExpr, new MemAbsAccess(8, new MemLabel(), atomExpr.value));
+        }
 
         return null;
     }
 
     @Override
-    public Long visit(AstFunDefn.AstRefParDefn refParDefn, Integer depth) {
-        return AstFullVisitor.super.visit(refParDefn, depth);
+    public Object visit(AstCallExpr callExpr, Integer depth) {
+        //var fnDefn = SemAn.definedAt.get(callExpr);
+        long argSize = getSizeInBytes(SemPointerType.type); // We need at least static link for each function
+        for (var arg : callExpr.args) {
+            arg.accept(this, depth);
+
+            var type = SemAn.ofType.get(arg);
+            // Round parameters & arguments
+            long size = getRoundedSizeInBytes(type);
+            argSize += size;
+        }
+
+        // todo - ask - should we round the return size as well?
+        long returnSize = getRoundedSizeInBytes(SemAn.ofType.get(callExpr));
+
+        long callSize = max(argSize, returnSize);
+
+        this.maxCallSize = max(this.maxCallSize, callSize);
+
+        return null;
+    }
+
+
+    @Override
+    public Object visit(AstStrType strType, Integer depth) {
+        return processRecordType(strType);
     }
 
     @Override
-    public Long visit(AstFunDefn.AstValParDefn valParDefn, Integer depth) {
-        return AstFullVisitor.super.visit(valParDefn, depth);
+    public Object visit(AstUniType uniType, Integer depth) {
+        return processRecordType(uniType);
     }
 
-    @Override
-    public Long visit(AstArrExpr arrExpr, Integer depth) {
-        return AstFullVisitor.super.visit(arrExpr, depth);
-    }
+    private long processRecordType(AstRecType recType) {
+        long offset = 0;
+        for (var cmp : recType.cmps) {
+            cmp.accept(this, -1);
 
-    @Override
-    public Long visit(AstAtomExpr atomExpr, Integer depth) {
-        return AstFullVisitor.super.visit(atomExpr, depth);
-    }
+            var type = SemAn.ofType.get(cmp);
+            // Round the size in components
+            long size = getRoundedSizeInBytes(type);
 
-    @Override
-    public Long visit(AstBinExpr binExpr, Integer depth) {
-        return AstFullVisitor.super.visit(binExpr, depth);
-    }
+            // Depth is -1 for components of a record
+            long cmpOffset = recType instanceof AstStrType ? offset : 0;
+            var memAcc = new MemRelAccess(size, cmpOffset, -1);
+            Memory.cmpAccesses.put(cmp, memAcc);
 
-    @Override
-    public Long visit(AstCallExpr callExpr, Integer depth) {
-        return AstFullVisitor.super.visit(callExpr, depth);
-    }
+            offset += size;
+        }
 
-    @Override
-    public Long visit(AstCastExpr castExpr, Integer depth) {
-        return AstFullVisitor.super.visit(castExpr, depth);
-    }
-
-    @Override
-    public Long visit(AstCmpExpr cmpExpr, Integer depth) {
-        return AstFullVisitor.super.visit(cmpExpr, depth);
-    }
-
-    @Override
-    public Long visit(AstNameExpr nameExpr, Integer depth) {
-        return AstFullVisitor.super.visit(nameExpr, depth);
-    }
-
-    @Override
-    public Long visit(AstPfxExpr pfxExpr, Integer depth) {
-        return AstFullVisitor.super.visit(pfxExpr, depth);
-    }
-
-    @Override
-    public Long visit(AstSfxExpr sfxExpr, Integer depth) {
-        return AstFullVisitor.super.visit(sfxExpr, depth);
-    }
-
-    @Override
-    public Long visit(AstSizeofExpr sizeofExpr, Integer depth) {
-        return AstFullVisitor.super.visit(sizeofExpr, depth);
-    }
-
-    @Override
-    public Long visit(AstAssignStmt assignStmt, Integer depth) {
-        return AstFullVisitor.super.visit(assignStmt, depth);
-    }
-
-    @Override
-    public Long visit(AstBlockStmt blockStmt, Integer depth) {
-        return AstFullVisitor.super.visit(blockStmt, depth);
-    }
-
-    @Override
-    public Long visit(AstExprStmt exprStmt, Integer depth) {
-        return AstFullVisitor.super.visit(exprStmt, depth);
-    }
-
-    @Override
-    public Long visit(AstIfStmt ifStmt, Integer depth) {
-        return AstFullVisitor.super.visit(ifStmt, depth);
-    }
-
-    @Override
-    public Long visit(AstReturnStmt retStmt, Integer depth) {
-        return AstFullVisitor.super.visit(retStmt, depth);
-    }
-
-    @Override
-    public Long visit(AstWhileStmt whileStmt, Integer depth) {
-        return AstFullVisitor.super.visit(whileStmt, depth);
-    }
-
-    @Override
-    public Long visit(AstArrType arrType, Integer depth) {
-        return AstFullVisitor.super.visit(arrType, depth);
-    }
-
-    @Override
-    public Long visit(AstAtomType atomType, Integer depth) {
-        return AstFullVisitor.super.visit(atomType, depth);
-    }
-
-    @Override
-    public Long visit(AstNameType nameType, Integer depth) {
-        return AstFullVisitor.super.visit(nameType, depth);
-    }
-
-    @Override
-    public Long visit(AstPtrType ptrType, Integer depth) {
-        return AstFullVisitor.super.visit(ptrType, depth);
-    }
-
-    @Override
-    public Long visit(AstStrType strType, Integer depth) {
-        return AstFullVisitor.super.visit(strType, depth);
-    }
-
-    @Override
-    public Long visit(AstUniType uniType, Integer depth) {
-        return AstFullVisitor.super.visit(uniType, depth);
-    }
-
-    @Override
-    public Long visit(AstRecType.AstCmpDefn cmpDefn, Integer depth) {
-        return AstFullVisitor.super.visit(cmpDefn, depth);
+        return offset;
     }
 }
